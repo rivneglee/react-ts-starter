a4bbd98f074bc8c908f7a4cf0c64dd383fdfba97
CHENG LI <rivneg@outlook.com>
2021-11-18 22:07:18 +1100

    Add storybook

---
+++ src/stories/SimpleComponnt.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import SimpleComponent from '../components/SimpleComponent';
+export default {
+  component: SimpleComponent,
+  title: 'Components/SimpleComponent'
+};
+
+export const Primary = () => <SimpleComponent id="Simple"></SimpleComponent>;
